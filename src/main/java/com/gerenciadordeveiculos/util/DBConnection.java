113a90ed1ecbde803c00ee1f287967d2f9eb62ca
luizfelipecirqueira <lfcs18ts@gmail.com>
2025-01-29 10:47:03 -0300

    Modificação da senha do BD.

--- src/main/java/com/gerenciadordeveiculos/util/DBConnection.java
+++ src/main/java/com/gerenciadordeveiculos/util/DBConnection.java
@@ -16,7 +16,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/gerenciadordeveiculos";
     private static final String USER = "root";
-    private static final String PASSWORD = "Felipinho32@";
+    private static final String PASSWORD = "suaSenha";
 
     public static Connection getConnection() throws SQLException {
         try {
